3b7ef34adbbb47fa149eb008cf206d10d0197835
MOON <moonjs1018@naver.com>
2024-05-06 19:56:48 +0900

    1주차 프로그래머스 다리를 지나는 트럭

---
+++ 프로그래머스_다리를_지나는_트럭.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+
+        Queue<Integer> bridge = new LinkedList<>();
+        int sum = 0;
+        int time = 0;
+
+        for (int i = 0; i < truck_weights.length; i++) {
+            int truck = truck_weights[i];
+
+            while (true) {
+                if (bridge.isEmpty()) {
+                    bridge.add(truck);
+                    sum += truck;
+                    time++;
+                    break;
+                } else if (bridge.size() == bridge_length) { // 다리에 트럭이 전부 찬 경우
+                    sum -= bridge.poll();
+                } else { // 다리에 트럭 자리가 남았을 때
+                    if (sum + truck <= weight) {
+                        bridge.add(truck);
+                        sum += truck;
+                        time++;
+                        break;
+                    } else {
+                        bridge.add(0);
+                        time++;
+                    }
+                }
+            }
+        }
+        int result =time + bridge_length;
+        return result;
+    }
+}
