94c1f94020a2aeb49c9cec2cfd84ea342f4ec36e
MOON <moonjs1018@naver.com>
2024-05-09 22:34:05 +0900

    1주차 백준 9093 문제

--- 백준_9093_단어뒤집기.java
+++ 백준_9093_단어뒤집기.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class 백준_9033_단어뒤집기 {
+public class 백준_9093_단어뒤집기 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
