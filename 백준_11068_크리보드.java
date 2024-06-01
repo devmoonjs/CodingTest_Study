a1bc9560c70fdfac91da5906bda3f1ba1047d517
MOON <moonjs1018@naver.com>
2024-06-01 19:29:06 +0900

    4주차 백준 11068. 크리보드

---
+++ 백준_11068_크리보드.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class 백준_11068_크리보드 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        long[] dp = new long[N+1];
+
+        for (int i = 1; i <= Math.min(N, 6); i++) {
+            dp[i] = i;
+        }
+
+        for (int i = 7; i <= N; i++) {
+            for (int j = 2; j <= 5; j++) {
+                dp[i] = Math.max(dp[i-(j+1)] * j,  dp[i]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
